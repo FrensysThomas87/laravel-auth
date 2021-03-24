d783f7ae22fdc0056583262a33daf366302c2440
Francesco <delta873@gmail.com>
2021-03-24 17:25:23 +0100

    Creata inserita tabella film su index

--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -1,1 +1,26 @@
-ciao;
+@extends('movie.base')
+
+@section('index-content')
+<table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Title</th>
+        <th scope="col">Cover</th>
+
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ( $movie as $film )
+        <tr>
+            <th scope="row">{{$film->id}}</th>
+            <td>{{$film->title}}</td>
+            <td><img src="{{$film->cover}}" alt=""></td>
+
+          </tr>
+        @endforeach
+
+
+    </tbody>
+  </table>
+@endsection
